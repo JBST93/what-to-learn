aa152361978bda257b16558491ca66624148bd6f
Akash Zaveri <akazaveri@gmail.com>
2024-02-27 19:33:53 +0000

    working on new enrollment

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,2 +1,29 @@
 class EnrollmentsController < ApplicationController
+  # before_action :set_course, only: [:new, :create]
+
+  def new
+    @course = Course.find(params[:course_id])
+    @enrollment = Enrollment.new
+    @enrollment.course = @course
+    @enrollment.user = current_user
+  end
+
+  def create
+    @course = Course.find(params[:course_id])
+    @enrollment = Enrollment.new(enrollment_params)
+    @enrollment.user = current_user
+    if @enrollment.save
+      flash.alert = "Enrolled"
+    end
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
+
+  def enrollment_params
+    params.require(:enrollment).permit(:course_id)
+  end
 end
