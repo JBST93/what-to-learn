7b58b00ccb356135d3af125642e7795c62f4742f
Nicolas Ayoub <nicolas.ayoub1@gmail.com>
2024-02-27 18:03:56 +0000

    Created my pages show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-
-
-
   def my_courses
+    @my_courses = Enrollment.where(user: current_user)
+    raise
   end
 
+  private
+
+  def user_params
+    # params.require(:user).permit(:id)
+  end
 end
