82aeca9a9047ae1ca8a75ac55f1a526037f540ff
Nicolas Ayoub <nicolas.ayoub1@gmail.com>
2024-02-27 13:05:06 +0000

    Created courses controller

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,63 @@
+class CoursesController < ApplicationController
+  before_action :set_user, only: %i[new create]
+  before_action :set_course, only: [:show, :edit, :destroy]
+
+  def index
+    @courses = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    @course.user = @user
+
+    if @course.save
+      redirect_to course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @course = Course.update(course_params)
+
+    redirect_to course_path(@course)
+  end
+
+  def destroy
+    @course.destroy
+
+    redirect_to courses_path, status: :see_other
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title,
+                                   :description,
+                                   :start_date,
+                                   :end_date,
+                                   :price,
+                                   :active,
+                                   :tag,
+                                   :text_editor,
+                                   :user_id)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+end
