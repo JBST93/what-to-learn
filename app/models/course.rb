27b2780179d43138d8526c526ba1490d1f94b0f2
Nicolas Ayoub <nicolas.ayoub1@gmail.com>
2024-02-27 16:36:56 +0000

    Created validations and associations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :user
+  has_many :enrollments, dependent: :destroy
   has_one_attached :image
 
   validates :title, :start_date, :end_date, :price, presence: true
