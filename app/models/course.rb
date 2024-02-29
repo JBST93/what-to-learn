502e786a00181dbb24adc1c304137512a56c2856
Andy Tutton <andytutton2@gmail.com>
2024-02-29 11:11:06 +0000

    reviews models made

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :user, foreign_key: 'course_creator_id'
   has_many :enrollments, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   has_many :users, through: :enrollments
   has_one_attached :image
 
@@ -8,4 +9,5 @@ class Course < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   validates :title, :start_date, :end_date, :price, presence: true
+
 end
