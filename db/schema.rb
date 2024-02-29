32f103b16234fd0f23bb3688ec77b31718b2809f
Jacek Bastin <jacek.bastin@gmail.com>
2024-02-29 12:54:18 +0000

    Added date cards

--- db/schema.rb
+++ db/schema.rb
@@ -91,7 +91,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_144144) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "role", default: "Student"
+    t.string "role", default: "student"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
