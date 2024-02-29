7bb2df2ce09ff2857d21b9a682a33cf6ea890580
Jacek Bastin <jacek.bastin@gmail.com>
2024-02-29 11:23:34 +0000

    Created Filter + Styled it + fixed bug with layout of cards when filtering

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,6 +4,15 @@ class CoursesController < ApplicationController
 
   def index
     @courses = Course.all
+  if params[:query].present?
+
+    sql_query = <<~SQL
+    courses.title ILIKE :query
+    OR courses.description ILIKE :query
+    SQL
+
+    @courses = @courses.where(sql_query, query: "%#{params[:query]}%")
+    end
   end
 
   def show
