3dfd94187a97b4fe882df9996437577475f454a8
Nicolas Ayoub <nicolas.ayoub1@gmail.com>
2024-02-29 14:39:21 +0000

    Improved page logic student vs teacher

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   def my_enrollments
     @my_enrollments = Enrollment.where(user: current_user)
 
-    @user = current_user
-    @courses = @user.courses
+    @courses = current_user.courses
     respond_to do |format|
       format.html
       format.json { render json: @courses }
