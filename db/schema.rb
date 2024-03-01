47620e7b2692922fb6f9f86655575df256eb1b07
Nicolas Ayoub <nicolas.ayoub1@gmail.com>
2024-03-01 15:00:29 +0000

    Styled buttons and calendar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_03_01_130845) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,6 +86,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_01_130845) do
   create_table "messages", force: :cascade do |t|
     t.bigint "sender_id"
     t.bigint "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text "content"
     t.index ["recipient_id"], name: "index_messages_on_recipient_id"
     t.index ["sender_id"], name: "index_messages_on_sender_id"
