1c557e94e6834faa3c570dfa14cf57af916f3305
Akash Zaveri <akazaveri@gmail.com>
2024-03-01 16:36:59 +0000

    added styling

--- db/migrate/20240301125128_create_messages.rb
+++ db/migrate/20240301125128_create_messages.rb
@@ -3,7 +3,6 @@ class CreateMessages < ActiveRecord::Migration[7.1]
     create_table :messages do |t|
       t.references :sender
       t.references :recipient
-      t.timestamps
     end
     # Rails 5+ only: add foreign keys
     add_foreign_key :messages, :users, column: :sender_id, primary_key: :id
