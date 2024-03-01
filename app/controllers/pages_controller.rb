775d969c01951ccce5dd63093c94c3334b8493de
Jacek Bastin <jacek.bastin@gmail.com>
2024-03-01 11:15:44 +0000

    dissociated home and course index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @courses = Course.all
+    if params[:query].present?
+
+      sql_query = <<~SQL
+      courses.title ILIKE :query
+      OR courses.description ILIKE :query
+      SQL
+
+      @courses = @courses.where(sql_query, query: "%#{params[:query]}%")
+      end
   end
 end
