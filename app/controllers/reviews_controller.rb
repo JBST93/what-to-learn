06f601fcad8c15b3df0cc3242d992028678d9333
Andy Tutton <andytutton2@gmail.com>
2024-03-01 14:44:46 +0000

    new stuff

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,11 +5,13 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
+    @review.user = current_user
   end
 
   def create
     @review = Review.new(review_params)
     @review.course_id = params[:course_id]
+    @review.user = current_user
     if @review.save
       redirect_to course_path(@review.course)
     else
